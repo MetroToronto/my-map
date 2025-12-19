c228359da3d02ee81bbe790e17cf591d1be98450
Zain Khurram <zain.ca@gmail.com>
2025-12-19 08:37:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,13 @@
   'use strict';
 
   // --------------------- Map boot ---------------------
-  const map = L.map('map').setView([43.92705, -79.5299], 11);
+  const map = L.map('map').setView([43.96169, -79.750271], 11);
   window.map = map; // required by routing.js / report.js
 
   map.zoomControl.setPosition('topright');
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 12,
+    maxZoom: 10,
     attribution: '© OpenStreetMap'
   }).addTo(map);
 
